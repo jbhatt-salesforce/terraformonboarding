c2fc76686df42eefd88b837392debe979ebac4e0
jbhatt <jbhatt@salesforce.com>
2018-09-14 17:20:18 -0700

    collecting pgpkey info and teamname, loading them into variables

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 # This provider is only temporarily needed. We'll remove it in a future step.
 provider "aws" {}
 
-module "my-terraform-environment" {
+module "<YOUR_TEAM_NAME_HERE>-terraform-environment" {
   source         = "git@github.com:sfdc-pcg/terraform-aws-tfstate.git//environment?ref=1.2.0"
-  name           = "<YOUR_TERRAFORM_ENVIRONMENT_NAME_HERE>"
+  name           = "<YOUR_TEAM_NAME_HERE>"
   public_ssh_key = "<YOUR_TERRAFORM_SSH_KEY_HERE>"
   pgp_key        = "<YOUR_PGP_KEY_HERE>"
   bootstrap      = true
--- tfonboard.py
+++ tfonboard.py
@@ -7,26 +7,40 @@ import sys
 import shutil
 
 #Step 2: Setup a Dockerfile
-#command line argument to gather the terraform github account sshkey.
+#command line argument to gather the terraform github account sshkey and pgp key.
 parser = argparse.ArgumentParser()
 parser.add_argument("sshkeypath", help='Path to the ssh key file for the terraform service account e.g. /Users/username/.ssh/id_rsa', type=str)
+parser.add_argument("--pgpkeypath", help='Path to the base64 encoded *binary* PGP key.', type=str)
+parser.add_argument("--keybaseuname", help='Keybase username', type=str)
+parser.add_argument("teamname", help='Your team name', type=str)
+
+#If neither of the two optional args, pgpkeypath or keybaseuname is provided, exit the program
 args = parser.parse_args()
-src_private = os.path.expanduser(args.sshkeypath)
-src_public = src_private + ".pub"
+if not (args.pgpkeypath or args.keybaseuname):
+    print ("Must provide either pgpkeypath or keybaseuname. Try -h for more info.")
+    sys.exit(0)
+
+#expand the path if needed
+src_privatekey = os.path.expanduser(args.sshkeypath)
+src_publickey = src_privatekey + ".pub"
 dst_privatekey = os.getcwd() + "/id_rsa"
 dst_publickey = os.getcwd() + "/id_rsa.pub"
 
 #copy the private and public keys from the args.sshkeypath to the current directory
-if os.path.exists(src_private):
-    print ("Copying private and public key from " + src_private + " to " + dst_privatekey)
-    shutil.copy(src_private, dst_privatekey)
-    shutil.copy(src_public, dst_publickey)
+if os.path.exists(src_privatekey):
+    print ("Copying private and public key from " + src_privatekey + " to " + dst_privatekey)
+    shutil.copy(src_privatekey, dst_privatekey)
+    shutil.copy(src_publickey, dst_publickey)
+    with open(dst_publickey, 'r') as pubsshkeyfile:
+       pubsshkeydata=pubsshkeyfile.read().replace('\n', '')
+       print ("Your public ssh key is: " + pubsshkeydata + "\n")
 
 #Make sure that the Dockerfile exists
 if os.path.isfile("Dockerfile"):
     print ("Found the Dockerfile...")
 else:
    print ("Either the Dockerfile is missing or not readable")
+
 #create a directory called terraform 
 if not os.path.exists("./terraform"):
    print ("Creating terraform directory...")
@@ -40,3 +54,22 @@ if os.path.isfile("main.tf"):
 else:
     print ("Could not find main.tf file")
 
+#copy pgp key file to the current folder
+if args.pgpkeypath:
+   src_pgpkey = os.path.expanduser(args.pgpkeypath)
+   dst_pgpkey = os.getcwd()+'/pgpkey.asc'
+   if os.path.exists(src_pgpkey):
+      #print("Copying pgpkey from " + src_pgpkey + " to " + dst_pgpkey)
+      #shutil.copy(src_pgpkey, dst_pgpkey)
+      with open(src_pgpkey, 'r') as pgpfile:
+         pgpkeydata=pgpfile.read().replace('\n', '')
+         print("Your pgp key: " + pgpkeydata + "\n")
+
+#if keybase is provided load that into a variable
+if args.keybaseuname:
+    print ("Your keybase username is: " + args.keybaseuname) 
+    keybase_username = args.keybaseuname
+
+#open and read main.tf template
+#with open("main.tf", 'rw') as maintffile:
+    
